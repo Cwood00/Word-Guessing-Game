897570600186e498d450a8cf7ad6ebc094bb5360
Chris Wood <118241499+Cwood00@users.noreply.github.com>
2023-11-12 22:04:55 -0600

    Too sleepy, will finish later

--- ServerProgram/src/main/java/Server.java
+++ ServerProgram/src/main/java/Server.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.function.Consumer;
 
 //Back end of server application
@@ -30,13 +34,85 @@ public class Server{
         @Override
         public void run(){
             GUIprinter.accept("Server is listening for clients");
+            try{
+                while (true){
+                    clientHandlerThread newClient = new clientHandlerThread(listeningSocket.accept());
+                    newClient.start();
+                    clients.add(newClient);
+                }
+            }
+            catch(IOException e){
+                GUIprinter.accept("IO exception in accept loop");
+            }
         }
-    }
+    }//End class ListeningThread
     //Plays game with one client
     private class clientHandlerThread extends Thread{
+        Socket connection;
+        ObjectInputStream in;
+        ObjectOutputStream out;
+        private ArrayList<String> categories;
+        private HashMap<String, ArrayList<String>> words;
+        private HashMap<String, Integer> failedGuesses;
+        private int solvedCategories;
+        private boolean playerHasWon;
+        private boolean playerHasLost;
 
+        public clientHandlerThread(Socket incomingConnection){
+            connection = incomingConnection;
+
+            try {
+                out = new ObjectOutputStream(connection.getOutputStream());
+                in = new ObjectInputStream(connection.getInputStream());
+                connection.setTcpNoDelay(true);
+            }
+            catch (IOException e){
+                GUIprinter.accept("Failed to establish I/O streams");
+            }
+            populateWordsMaps();
+
+            solvedCategories = 0;
+            playerHasWon = false;
+            playerHasLost = false;
+        }
         @Override
         public void run(){
+            try {
+                GUIprinter.accept("Sending categories to user");
+                out.writeObject(categories);
+                while(!playerHasWon && !playerHasLost){
+                    //TODO play game
+                }
+            }
+            catch (IOException e){
+                GUIprinter.accept("Client has disconnected");
+            }
+        }
+        private void populateWordsMaps(){
+            categories.add("Category1");
+            categories.add("Category2");
+            categories.add("Category3");
+
+            ArrayList<String> Category1Words = new ArrayList<>();
+            Category1Words.add("Cat1Word1");
+            Category1Words.add("Cat1Word2");
+            Category1Words.add("Cat1Word3");
+            words.put(categories.get(0), Category1Words);
+            failedGuesses.put(categories.get(0), 0);
+
+            ArrayList<String> Category2Words = new ArrayList<>();
+            Category2Words.add("Cat2Word1");
+            Category2Words.add("Cat2Word2");
+            Category2Words.add("Cat2Word3");
+            words.put(categories.get(1), Category2Words);
+            failedGuesses.put(categories.get(1), 0);
+
+            ArrayList<String> Category3Words = new ArrayList<>();
+            Category3Words.add("Cat3Word1");
+            Category3Words.add("Cat3Word2");
+            Category3Words.add("Cat3Word3");
+            words.put(categories.get(2), Category3Words);
+            failedGuesses.put(categories.get(2), 0);
 
         }
     }
--- ServerProgram/src/test/java/ServerTests.java
+++ ServerProgram/src/test/java/ServerTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 class ServerTests{
 
@@ -14,7 +17,7 @@ class ServerTests{
 		callBackReturn = "";
 	}
 	@Test
-	void serverAcceptsClients(){
+	void serverListensForClients(){
 		Server server = new Server(55555, s->{
 			//no-op
 		});
@@ -33,10 +36,32 @@ class ServerTests{
 		});
 		try{
 			Socket clientSocket = new Socket("127.0.0.1", 55556);
+			//Put the tread to sleep, to wait for server call back response
+			try {Thread.sleep(100);}
+			catch (InterruptedException e){/* no-op */}
 			assertEquals("Server is listening for clients", callBackReturn);
 		}
 		catch (IOException e){
 			fail("Could not connect to server");
+		}
+	}
+	@Test
+	void serverSendsCategories(){
+		Server server = new Server(55557, s->{
+			this.callBackReturn = s.toString();
+		});
+		try {
+			Socket clientSocket = new Socket("127.0.0.1", 55557);
+			ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+			clientSocket.setTcpNoDelay(true);
+			ArrayList<String> categories = (ArrayList<String>)in.readObject();
+			assertEquals(3, categories.size());
+		}
+		catch (IOException e) {
+			fail("Could not connect to the server");
+		}
+		catch (ClassNotFoundException e){
+			fail("Server sent bad class");
 		}
 	}
 }
